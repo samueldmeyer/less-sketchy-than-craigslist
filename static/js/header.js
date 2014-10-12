7edcd40d9b6b51e7e4a9755dd23047f934163695
Samuel Meyer <zero@Samuels-MacBook-Pro.local>
2014-10-12 16:03:16 -0700

    wrapping js to avoid globals

--- static/js/header.js
+++ static/js/header.js
@@ -1,27 +1,29 @@
-angular.module('lstc.header', [])
-.controller('HeaderController', ['$location', function($location) {
-  this.menuIsOpen = false;
-  this.toggleMenu = function() {
-    this.menuIsOpen = !this.menuIsOpen;
-  };
-  this.isActive = function(viewLocation) {
-    return viewLocation === $location.path();
-  }
-}])
-.directive('appHeader', function(){
-  return {
-    restrict: 'E',
-    templateUrl: '/static/partials/app-header.html',
-    controller: ['$location', function($location) {
-      this.menuIsOpen = false;
-      this.toggleMenu = function() {
-        this.menuIsOpen = !this.menuIsOpen;
-      };
-      this.isActive = function(viewLocation) {
-        //tests if a view is active
-        return viewLocation === $location.path();
-      }
-    }],
-    controllerAs: 'header'
-  }
-});
+(function(){
+  var app = angular.module('lstc.header', []);
+  app.controller('HeaderController', ['$location', function($location) {
+    this.menuIsOpen = false;
+    this.toggleMenu = function() {
+      this.menuIsOpen = !this.menuIsOpen;
+    };
+    this.isActive = function(viewLocation) {
+      return viewLocation === $location.path();
+    }
+  }]);
+  app.directive('appHeader', function(){
+    return {
+      restrict: 'E',
+      templateUrl: '/static/partials/app-header.html',
+      controller: ['$location', function($location) {
+        this.menuIsOpen = false;
+        this.toggleMenu = function() {
+          this.menuIsOpen = !this.menuIsOpen;
+        };
+        this.isActive = function(viewLocation) {
+          //tests if a view is active
+          return viewLocation === $location.path();
+        }
+      }],
+      controllerAs: 'header'
+    }
+  });
+})();
--- static/js/items.js
+++ static/js/items.js
@@ -1,73 +1,75 @@
 // This module includes all controllers and services for items, including displaying and adding
-angular.module('lstc.items', [])
-// Services
-.factory('ItemsApi', ['$resource', function($resource){
-  return $resource('/items/:id', {id: '@id'}, {});
-}])
-.factory('Items', ['$filter', 'ItemsApi', function($filter, ItemsApi){
-  var fac = {};
-  fac.currentItem = {};
+(function(){
+  var app = angular.module('lstc.items', []);
+  // Services
+  app.factory('ItemsApi', ['$resource', function($resource){
+    return $resource('/items/:id', {id: '@id'}, {});
+  }]);
+  app.factory('Items', ['$filter', 'ItemsApi', function($filter, ItemsApi){
+    var fac = {};
+    fac.currentItem = {};
 
-  fac.all = ItemsApi.query();
+    fac.all = ItemsApi.query();
 
-  fac.fullItems = [];
+    fac.fullItems = [];
 
-  fac.setCurrent = function(id) {
-    // Sets item based on item id if not already done
-    if ($filter('filter')(fac.fullItems, {id: id})[0] !== this.currentItem) {
-      angular.copy($filter('filter')(fac.fullItems, {id: id})[0], this.currentItem);
+    fac.setCurrent = function(id) {
+      // Sets item based on item id if not already done
+      if ($filter('filter')(fac.fullItems, {id: id})[0] !== this.currentItem) {
+        angular.copy($filter('filter')(fac.fullItems, {id: id})[0], this.currentItem);
+      }
+      // If item is not in the current list in memory, get from server
+      if (!this.currentItem.id) {
+        this.currentItem = ItemsApi.get({id: id});
+        this.fullItems.push(this.currentItem);
+      }
+    };
+
+    fac.addItem = function(item, success) {
+      //success is an optional function that accepts an item returned from the server as input
+      // newItem = ItemsApi.createInstance([item]);
+      var addToListAndSuccess = function (item) {
+        fac.all.push(item);
+        fac.fullItems.push(item);
+        (success || angular.noop)(item);
+      }
+      ItemsApi.save(item, addToListAndSuccess);
     }
-    // If item is not in the current list in memory, get from server
-    if (!this.currentItem.id) {
-      this.currentItem = ItemsApi.get({id: id});
-      this.fullItems.push(this.currentItem);
-    }
-  };
 
-  fac.addItem = function(item, success) {
-    //success is an optional function that accepts an item returned from the server as input
-    // newItem = ItemsApi.createInstance([item]);
-    var addToListAndSuccess = function (item) {
-      fac.all.push(item);
-      fac.fullItems.push(item);
-      (success || angular.noop)(item);
-    }
-    ItemsApi.save(item, addToListAndSuccess);
-  }
-
-  return fac;
-}])
+    return fac;
+  }]);
 
 
 
-// Controllers
-.controller('AllItemsController', ['Items', function (Items) {
-  this.items = Items.all;
-}])
-.controller('SingleItemController', ['Items', '$routeParams', function (Items, $routeParams) {
-  this.item = {};
+  // Controllers
+  app.controller('AllItemsController', ['Items', function (Items) {
+    this.items = Items.all;
+  }]);
+  app.controller('SingleItemController', ['Items', '$routeParams', function (Items, $routeParams) {
+    this.item = {};
 
-  this.init = function() {
-    Items.setCurrent($routeParams.itemId);
-    this.item = Items.currentItem;
-  };
+    this.init = function() {
+      Items.setCurrent($routeParams.itemId);
+      this.item = Items.currentItem;
+    };
 
-  this.emailTab = function() {
-    // open email in a new tab/window
-    window.open("mailto:" + encodeURIComponent(this.item.email) + "?subject=" + encodeURIComponent(this.item.title));
-  };
+    this.emailTab = function() {
+      // open email in a new tab/window
+      window.open("mailto:" + encodeURIComponent(this.item.email) + "?subject=" + encodeURIComponent(this.item.title));
+    };
 
-  this.init();
-}])
-.controller('SellFormController', ['Items', '$location', function(Items, $location){
-  this.sell = {};
+    this.init();
+  }]);
+  app.controller('SellFormController', ['Items', '$location', function(Items, $location){
+    this.sell = {};
 
-  this.addItem = function() {
-    var ctrl = this;
-    Items.addItem(this.sell, function(item) {
-      ctrl.sell = {};
-      $location.path( "/listedstuff/" + item.id );
-    });
-  };
-  
-}]);
+    this.addItem = function() {
+      var ctrl = this;
+      Items.addItem(this.sell, function(item) {
+        ctrl.sell = {};
+        $location.path( "/listedstuff/" + item.id );
+      });
+    };
+    
+  }]);
+})();
